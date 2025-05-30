15da025aafb817bc286830b4baf26c7ca48f2937
erenfn <erenfindik7@gmail.com>
2025-05-30 21:00:48 +0300

    change api base url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,11 +1,11 @@
 // API constants
 //local environment
-export const BASE_URL = 'localhost:3000';
-export const API_BASE_URL = `http://${BASE_URL}/api/`;
+// export const BASE_URL = 'localhost:3000';
+// export const API_BASE_URL = `http://${BASE_URL}/api/`;
 
 //staging environment
-// export const BASE_URL = 'onboarding-demo.bluewavelabs.ca';
-// export const API_BASE_URL = `https://${BASE_URL}/api/`;
+export const BASE_URL = 'onboarding-demo.bluewavelabs.ca';
+export const API_BASE_URL = `https://${BASE_URL}/api/`;
 // Other constants
 export const APP_TITLE = 'Bluewave Onboarding';
 export const SUPPORT_EMAIL = 'support@bluewave.com';
