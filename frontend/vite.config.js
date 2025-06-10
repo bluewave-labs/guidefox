a9ae5f9d643c625c5701794386464e50b6774062
Debora Serra <debora.r.serra@gmail.com>
2025-06-10 06:28:07 -0700

    docs: update vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 import { BASE_URL } from "./src/utils/constants";
 
 export default defineConfig({
